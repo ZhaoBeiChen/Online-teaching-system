71bd3e1e68e2900e4e360347a6c7b35766ff6c1e
Destinyside <destinyside@163.com>
2017-12-18 16:10:45 +0800

    base dao modified

--- src/main/java/DAO/BaseDAO.java
+++ src/main/java/DAO/BaseDAO.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public interface BaseDAO<T>
 {
-    List<T> get(String table);
+    T get(String sql);
+    List<T> getBySQL(String sql);
     boolean save(T t);
     boolean update(T t);
     boolean delete(T t);
     Object get(Class type, Serializable id);
+    T getById(Class<T> cls,Serializable id);
     Object load(Class type, Serializable id);
-    T findById(Class<T> cls,Serializable id);
-    List<T> findBySQL(String sql);
     void init();
     boolean clear();
 }
