c0b892e6fe702aef04627e94c346cbfa32f441ca
Destinyside <destinyside@163.com>
2017-12-19 09:14:52 +0800

    runnable test

---
+++ src/main/java/Controller/testAction.java
@@ -0,0 +1,15 @@
+package Controller;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author Zhudewen
+ */
+public class testAction extends ActionSupport
+{
+    public String Test()
+    {
+        return "success";
+    }
+}
